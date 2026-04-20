221fb2832f49798f4d9f3dbd036bd85d00e3a449
IHZ <Muhammad339163@gmail.com>
2026-04-20 23:05:53 +0800

    Html 2.0 I think

--- script.js
+++ script.js
@@ -2,4 +2,11 @@ const toggleButton = document.getElementById('theme-toggle');
 
 toggleButton.addEventListener('click', () => {
     document.body.classList.toggle('light-mode');
+    
+    // Change icon based on mode
+    if (document.body.classList.contains('light-mode')) {
+        toggleButton.innerText = '☀️';
+    } else {
+        toggleButton.innerText = '🌙';
+    }
 });
